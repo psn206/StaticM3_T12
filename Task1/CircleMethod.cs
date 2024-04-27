3bff033ee1e342d137568737a090f346c4febeca
Sergei Prutsakov <psn206@mail.ru>
2024-04-27 20:04:53 +0300

    Добавьте файлы проекта.

---
+++ Task1/CircleMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal static class CircleMethod
+    {
+
+        public static double GetLength(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        public static double GetArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static string BelongTo(double radius, double x, double y)
+        {
+            double r = Math.Sqrt(x * x + y * y);
+            return radius >= r ? "Точка пренадлежит окружности" : "Точка не пренадлежит окружности";
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности с координатами центра (0, 0)");
+            Console.Write("R=");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Длина окуржности: {CircleMethod.GetLength(radius):f} " +
+                $"Площадь:{CircleMethod.GetArea(radius):f}");
+
+            Console.WriteLine("Введите координаты точки проверяющий принадлежность к кругу");
+            Console.Write("X=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y=");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(CircleMethod.BelongTo(radius, x, y));
+            Console.ReadKey();
+        }
+    }
+}
